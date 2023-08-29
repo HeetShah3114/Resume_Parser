6cb93acbe4fda4946c0c2d0aca75cdbd8a041ba1
Heet Shah <126143094+HeetShah3114@users.noreply.github.com>
2023-08-29 17:41:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ nlp = spacy.load('en_core_web_sm')
 app = Flask(__name__)
 
 # Configure the upload folder
-UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
+UPLOAD_FOLDER = templates/UPLOAD_FOLDER
 ALLOWED_EXTENSIONS = {'pdf', 'docx'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
